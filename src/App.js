32d8ca6711225a824e42627c59d20f5e9efe7598
Karthik <karthiky2410@gmail.com>
2025-06-26 16:20:33 +0530

    Stage and commit changes after stash pop

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SignUp from "./pages/SignIn.js";
 import ForgotPassword from "./pages/ForgotPassword.js";
 import ForgotId from "./pages/ForgotId.js";
 import Finish from "./pages/Finish.js";
-import DocumentUpload from "./pages/DocumentUpload.js";
+import PartAForm from "./pages/PartAForm.js";
 
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/forgot-id" element={<ForgotId />} />
         <Route path="/finish" element={<Finish />} />
-        <Route path="/upload-document" element={<DocumentUpload />}/>
+         <Route path="/part-a" element={<PartAForm />} />
       </Routes>
     </div>
   );
