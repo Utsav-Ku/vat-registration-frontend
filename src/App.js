a8353f2e49323d0b335ce606948ead7458937355
Karthik <karthiky2410@gmail.com>
2025-06-29 13:48:10 +0530

    refactor: Remove PartBForm and related code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import ForgotId from "./pages/ForgotId.js";
 import Finish from "./pages/Finish.js";
 import DocumentUpload from "./pages/DocumentUpload.js";
 import PartAForm from "./pages/PartAForm.js";
-import PartBForm from './pages/PartBForm';
 import BusinessPartnerDetails from "./pages/businessPartnerDetails.js";
 import PartCForm from "./pages/PartCForm.js";
 import BankInfo from "./pages/BankInfo.js";
@@ -23,7 +22,6 @@ function App() {
         <Route path="/finish" element={<Finish />} />
         <Route path="/upload-document" element={<DocumentUpload />}/>
         <Route path="/part-a" element={<PartAForm />} />
-        <Route path="/part-b" element={<PartBForm />} />
         <Route path="/part-c" element={<PartCForm />} />
         <Route path="/bank-info" element={<BankInfo />} />
         <Route path="/business-partner-details" element={<BusinessPartnerDetails />} />
